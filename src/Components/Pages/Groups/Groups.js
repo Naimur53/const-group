017d48d32be7d11a58891c68102f92e492c1f168
naimur53 <naimurrhman53@gmail.com>
2023-07-22 17:08:36 +0600

    eon

--- src/Components/Pages/Groups/Groups.js
+++ src/Components/Pages/Groups/Groups.js
@@ -1,38 +1,69 @@
-import { Box, Container, Grid } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { allGroup, selectData, setGpInfo } from '../../../features/data/dataSlice';
-import GroupCard from '../../SmallComponents/GroupCard/GroupCard';
-import AddIcon from '@mui/icons-material/Add';
+import { Box, Container, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import {
+  allGroup,
+  selectData,
+  setGpInfo,
+} from "../../../features/data/dataSlice";
+import GroupCard from "../../SmallComponents/GroupCard/GroupCard";
+import AddIcon from "@mui/icons-material/Add";
+
 const Groups = () => {
-    const dispatch = useDispatch()
-    const data = useSelector(selectData);
-    useEffect(() => {
-        dispatch(allGroup());
-        dispatch(setGpInfo({}));
-    }, [dispatch])
-    return (
-        <div className=' '>
-            <Container>
-                <Grid container spacing={4}>
-                    <Grid item xs={12} md={4} >
-                        <div className='h-full  w-full p-2 '>
-                            <Box component={NavLink} to='/group/create' style={{ backgroundColor: '#ffffff26' }} className=' flex justify-center sm:py-3 items-center w-full h-full rounded-md'>
-                                <div>
-                                    <AddIcon sx={{ fontSize: 100 }}></AddIcon>
-                                    <h1 className='text-xl text-center'>Create Group</h1>
-                                </div>
-                            </Box>
-                        </div>
-                    </Grid>
-                    {
-                        data.groups.map(sData => <GroupCard key={sData._id} info={sData}></GroupCard>)
-                    }
-                </Grid>
-            </Container>
-        </div>
-    );
+  const dispatch = useDispatch();
+  const data = useSelector(selectData);
+
+  // Step 1: State for Search Query
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    dispatch(allGroup());
+    dispatch(setGpInfo({}));
+  }, [dispatch]);
+
+  // Step 3: Filter Data Based on Search Query
+  const filteredGroups = data?.groups?.filter((sData) =>
+    sData.gpName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className=" ">
+      <Container>
+        {/* Step 2: Create a Search Input */}
+        <input
+          type="text"
+          placeholder="Search by group name..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="border rounded-md p-2 mb-4 inline-block w-full mt-5 border-gray-900"
+          style={{ background: "#ffffff26" }}
+        />
+
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <div className="h-full  w-full p-2 ">
+              <Box
+                component={NavLink}
+                to="/group/create"
+                style={{ backgroundColor: "#ffffff26", minHeight: "270px" }}
+                className=" flex justify-center sm:py-3 items-center w-full h-full rounded-md"
+              >
+                <div>
+                  <AddIcon sx={{ fontSize: 100 }}></AddIcon>
+                  <h1 className="text-xl text-center">Create Group</h1>
+                </div>
+              </Box>
+            </div>
+          </Grid>
+          {/* Step 3 (Continued): Use the filteredGroups */}
+          {filteredGroups.map((sData) => (
+            <GroupCard key={sData._id} info={sData}></GroupCard>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
 };
 
 export default Groups;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,30 +1,30 @@
-import { Container, Grid } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { Outlet, } from 'react-router';
-import { getNotification, selectData } from '../../../features/data/dataSlice';
-import Profile from '../Profile/Profile';
+import { Container, Grid } from "@mui/material";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router";
+import { getNotification, selectData } from "../../../features/data/dataSlice";
+import Profile from "../Profile/Profile";
 const Home = () => {
-    const data = useSelector(selectData)
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (data.user?.email) {
-            dispatch(getNotification({ email: data.user?.email }));
-        }
-    }, [data.user])
-    return (
-        <Container maxWidth='xl'>
-            <Grid container spacing={1}>
-                <Grid sx={{ display: { xs: 'none', md: 'block' } }} item xs={12} md={3}>
-                    <div className="sticky top-20 left-0 overflow-y-scroll">
-                        <Profile></Profile>
-                    </div>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Outlet></Outlet>
-                </Grid>
-                <Grid item xs={12} md={3}>
+  const data = useSelector(selectData);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (data.user?.email) {
+      dispatch(getNotification({ email: data.user?.email }));
+    }
+  }, [data.user]);
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={1}>
+        <Grid sx={{ display: { xs: "none", md: "block" } }} item xs={12} md={3}>
+          <div className="sticky top-20 left-0 overflow-y-scroll">
+            <Profile></Profile>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <Outlet></Outlet>
+        </Grid>
+        {/* <Grid item xs={12} md={3}>
                     <div className="sticky top-20  left-0 overflow-y-scroll">
                         dfdfd
                         dfdfdfdfd
@@ -33,10 +33,10 @@ const Home = () => {
                         df
                         dfd
                     </div>
-                </Grid>
-            </Grid>
-        </Container>
-    );
+                </Grid> */}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Home;
